c1741825b20e22b3738c53e60d84b28165fdb8ff
MatheusRCastro <matheusrezendecastro@gmail.com>
2025-07-22 10:50:02 -0300

    Começo da página de login, mal foi começada os meninos não trabalahm são vagabundinhos

--- script.sql
+++ script.sql
@@ -1,16 +1,34 @@
-CREATE TABLE IF NOT EXISTS unif{
-	id_unif PRIMARY KEY;
-	data_inicio_unif data,
-	data_fim_unif data,
-	data_inicio_inscricao_delegado data,
-	data_fim_inscricao_delegado data,
-	data_inicio_inscricao_comite data,
-	data_fim_inscricao_comite data,
-	data_inicio_inscricao_staff data,
-	data_fim_inscricao_staff data
-};
+SET SEARCH_PATH to unif
 
-CREATE TABLE IF NOT EXISTS usuario{
+CREATE TABLE IF NOT EXISTS unif(
+	id_unif serial PRIMARY KEY,
+	data_inicio_unif date,
+	data_fim_unif date,
+	data_inicio_inscricao_delegado date,
+	data_fim_inscricao_delegado date,
+	data_inicio_inscricao_comite date,
+	data_fim_inscricao_comite date,
+	data_inicio_inscricao_staff date,
+	data_fim_inscricao_staff date
+);
+
+CREATE TABLE IF NOT EXISTS comite(
+	id_comite serial PRIMARY KEY,
+	id_unif int,
+	cpf_d1 varchar(14) NOT NULL,
+	cpf_d2 varchar(14) NOT NULL,
+	cpf_d3 varchar(14) NOT NULL,
+	cpf_d4 varchar(14) NOT NULL,
+	tipo_comite varchar(100),
+	nome_comite varchar(14),
+	data_comite date,
+	num_delegados int,
+	descricao_comite varchar(1000),
+	comite_aprovado boolean DEFAULT FALSE,
+	FOREIGN KEY (id_unif) REFERENCES unif(id_unif)
+);
+
+CREATE TABLE IF NOT EXISTS usuario(
 	cpf varchar(14) PRIMARY KEY,
 	nome varchar(100),
 	email varchar(100),
@@ -19,37 +37,46 @@ CREATE TABLE IF NOT EXISTS usuario{
 	telefone varchar(14),
 	senha varchar(20),
 	instituicao varchar(100),
-	adm boolean set false 
-};
+	adm boolean DEFAULT false 
+);
 
-CREATE TABLE IF NO EXISTS delegado{
-	
-};
+CREATE TABLE IF NOT EXISTS delegado(
+	cpf varchar(14) PRIMARY KEY,
+	id_comite int,
+	representacao int,
+	comite_desejado int,
+	primeira_op_representacao int,
+	segunda_op_representacao int,
+	terceira_op_representacao int,
+	segunda_op_comite int,
+	terceira_op_comite int,
+	FOREIGN KEY (id_comite) REFERENCES comite(id_comite),
+	FOREIGN KEY (cpf) REFERENCES usuario(cpf)
+);
 
-CREATE TABLE IF NOT EXISTS delegacao{
-	id_delegacao serial PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS delegacao(
+	id_delegacao serial,
 	id_unif int,
 	cpf varchar(14),
 	verificacao_delegacao boolean,
 	FOREIGN KEY (cpf) REFERENCES usuario(cpf),
-	FOREIGN KEY (id_unif) REFERENCES unif(id_unif)
-	
-};
+	FOREIGN KEY (id_unif) REFERENCES unif(id_unif),
+	PRIMARY KEY (id_delegacao,id_unif)
+);
 
-CREATE TABLE IF NOT EXISTS secretario{
+CREATE TABLE IF NOT EXISTS secretario(
 	cpf varchar(14) PRIMARY KEY,
 	funcao varchar(30),
 	id_unif INT,
 	FOREIGN KEY (cpf) REFERENCES usuario(cpf),
 	FOREIGN KEY (id_unif) REFERENCES unif(id_unif)
-};
+);
 
-CREATE TABLE IF NOT EXISTS staff{
+CREATE TABLE IF NOT EXISTS staff(
 	cpf varchar(14) PRIMARY KEY,
 	id_unif INT,
 	justificativa varchar(500),
-	inscricao_aprovada boolean set false,
+	inscricao_aprovada boolean DEFAULT FALSE ,
 	FOREIGN KEY (id_unif) REFERENCES unif(id_unif),
-	FOREIGN KEY (cpf) REFERENCES usuario(cpf),
-};
-
+	FOREIGN KEY (cpf) REFERENCES usuario(cpf)
+);
