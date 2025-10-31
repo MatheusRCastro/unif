7cead900989660e3bb5348dd953f9b686f570e17
MatheusRCastro <matheusrezendecastro@gmail.com>
2025-10-31 10:57:56 -0300

    .

--- pages/inscricaoMesa.php
+++ pages/inscricaoMesa.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
-session_start();
-=======
   session_start();
->>>>>>> 8e08e1d (verificação de sessions concluida)
 ?>
 
 <!DOCTYPE html>
@@ -12,11 +8,6 @@ session_start();
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<<<<<<< HEAD
-  <script src="scripts/inscricaoMesa.js" defer></script>
-=======
-
->>>>>>> 8e08e1d (verificação de sessions concluida)
   <title>Inscrever Mesa Diretora</title>
   <link rel="stylesheet" href="styles/inscriçãoMesa.css" />
   <link rel="stylesheet" href="styles/global.css" />
@@ -29,52 +20,6 @@ session_start();
   if (isset($_SESSION["cpf"])) {
   ?>
 
-<<<<<<< HEAD
-    <div id="caixa">
-      <img src="images/unif.png" alt="imagem da unif" id="imagem" />
-    </div>
-
-    <div class="right-box">
-      <div class="container">
-        <h3>Dados dos diretores</h3>
-        <p>DIRETOR 1</p>
-        <input type="text" placeholder="CPF do diretor 1" />
-        <p>DIRETOR 2</p>
-        <input type="text" placeholder="CPF do diretor 2" />
-        <p>DIRETOR 3</p>
-        <input type="text" placeholder="CPF do diretor 3" />
-      </div>
-
-      <div class="container">
-        <h3>Especificações do comitê</h3>
-        <input type="text" placeholder="Tipo do comitê (ex.: CSNU)" />
-        <input type="text" placeholder="Nome do comitê" />
-        <input type="text" placeholder="Número de delegados" />
-        <input type="text" placeholder="Data histórica do comitê" />
-        <textarea name="descricao_comite" id="descricaoComite" rows="10"
-          placeholder="Digite aqui a descrição do comitê..."></textarea>
-
-        <p>O COMITE É EM DUPLA?</p>
-        <div class="dupla-btns">
-          <button>Sim</button>
-          <button>Não</button>
-        </div>
-
-        <button class="submit-btn">Submeter</button>
-      </div>
-    </div>
-
-  <?php
-  } else {
-    echo "Usuário não autenticado!";
-  ?>
-    <a href="login.html" class="erro-php">Se identifique aqui</a>
-  <?php
-
-  }
-
-  ?>
-=======
   <div id="caixa">
     <img src="images/unif.png" alt="imagem da unif" id="imagem" />
   </div>
@@ -120,7 +65,6 @@ session_start();
 
 ?>
 
->>>>>>> 8e08e1d (verificação de sessions concluida)
 </body>
 
 </html>
