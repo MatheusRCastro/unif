a872068f6d82175d8c57464af7f4e6cf99d202da
tutuuzn <arthur1337art@gmail.com>
2025-12-08 16:51:11 -0300

    ainda tem erro na pagina inscricaoMesa

--- pages/inscricaoMesa.php
+++ pages/inscricaoMesa.php
@@ -10,8 +10,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Inscrição - Mesa Diretora | UNIF XXXX</title>
   <link rel="stylesheet" href="styles/global.css" />
-  <link rel="stylesheet" href="styles/inscricaoMesa.css" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <link rel="stylesheet" href="styles/inscriçãoMesa.css" />
+  <link rel="stylesheet" href="https://cdnjs.oudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
 <body>
